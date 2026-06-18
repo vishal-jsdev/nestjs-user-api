7827ffd445b1134abcdd1bf54c61270dc975320a
Vishal Amrutiya <vishala.jsdev@gmail.com>
2026-06-18 17:22:54 +0530

    Resolved the comment

--- src/users/dtos/page-query.dto.ts
+++ src/users/dtos/page-query.dto.ts
@@ -1,11 +1,10 @@
-import { Type } from "class-transformer";
-import { isNotEmpty, IsNumber, IsOptional, IsPositive } from "class-validator";
+import { IsOptional, IsPositive } from "class-validator";
 
 export class PageQueryDto{
     @IsOptional()
     @IsPositive()
     limit:number
-    
+
     @IsOptional()
     @IsPositive()
     page:number
